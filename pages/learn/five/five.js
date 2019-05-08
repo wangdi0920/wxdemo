18f7cfb42065d99d629ecb83644fbf87645b8c76
di wang <wangdi@bmsoft.com.cn>
2019-05-08 17:01:04 +0800

    添加滚动

--- pages/learn/five/five.js
+++ pages/learn/five/five.js
@@ -11,24 +11,24 @@ Page({
     interval: 5000,
     duration: 1000
   },
-  changeIndicatorDots(e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-  changeAutoplay(e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-  intervalChange(e) {
-    this.setData({
-      interval: e.detail.value
-    })
-  },
-  durationChange(e) {
-    this.setData({
-      duration: e.detail.value
-    })
-  }
+  // changeIndicatorDots(e) {
+  //   this.setData({
+  //     indicatorDots: !this.data.indicatorDots
+  //   })
+  // },
+  // changeAutoplay(e) {
+  //   this.setData({
+  //     autoplay: !this.data.autoplay
+  //   })
+  // },
+  // intervalChange(e) {
+  //   this.setData({
+  //     interval: e.detail.value
+  //   })
+  // },
+  // durationChange(e) {
+  //   this.setData({
+  //     duration: e.detail.value
+  //   })
+  // }
 })
