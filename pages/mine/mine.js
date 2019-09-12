97ed3612a12a1c41da222d7bef77fcc4f68f1ca4
di wang <wangdi@bmsoft.com.cn>
2019-09-12 09:33:35 +0800

    修改

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,5 +1,11 @@
 // pages/mine/mine.js
 const app = getApp();
+const { numberReplace } = require('../../utils/util.js')
+let params = {
+  encryptedData: "D4yUWiasFXWLU6YMPADg799K5Y7aeeMZp7hXSi7ZjUTUJdUHfnQIB6HlccqMbHBOtVSpGVpLUBKnfJzmaM2PfSWZCH57BEYehEsHG9CV+LJaH9SHuLlshw64E/iDLoUK6jlVjIFNkf3CaxdnVHp3uVSNulF2Oc0Fz4FPmCNhf+qaZhq8+Q8PotI+plkEAs7uELUMI/wdZJMLs0W/rzCXN1uBsJLaxcz9WEkqagiiSPOjMnSW4t+leEoI2RaMvHKkeihJ/Ud0M46T5d8EWC9Pmy1e9IN82cXHcnk1Ij/kXTVfkF7ND76HtmI3uRvzcyscxdAtIHtyHbeZXubmd9SBogsU0ZKAFItkn6tFShWHdRyRnX2bxmZHvUcfYIkmxmmUBCKbjDpVVW1dZ0ufgeH0U+Yfc2xxHeIjNtd77/ins6AYSPxVR3elb2KHlwRzCdnPV09oXro5KLJYzB0L795hVKe0IXE/8M6y0aVRgW8Bn+M=",
+    ivStr: "y0yR1bhykHje9r58rbte7Q==",
+  token: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsb2dpbiIsInNlc3Npb25LZXkiOiJ4NUhkVEtqMDZjQk1QV2lWZUM1TnR3PT0iLCJvcGVuSWQiOiJvRU1ZNjVIX0hDWUs0RlBOa1BSUmFzVHhRZzZnIiwiZXhwIjoxNTYyNjQxNjc4LCJpYXQiOjE1NjI2MzgwNzgsImp0aSI6ImNkNjE5MzMxLWZlOWYtNGM2Yi04YWUyLTI1N2RjNjJmMjE1ZSJ9.vl5cNAfq8bH4QYS2QipubNmhBrnPTcSmzrFtf59TsZ4"
+}
 Page({
 
   /**
@@ -8,7 +14,8 @@ Page({
   data: {
      isLogin:false,
      userInfo:'',
-     isShow: false
+     isShow: false,
+     number:''
   },
 
   /**
@@ -22,9 +29,60 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       isLogin: true
-    })
-  
+    }) 
   },
+  getOpenid: function () {
+    let that = this;  //获取openid不需要授权
+    wx.login({
+      success: function (res) {    //请求自己后台获取用户openid
+        wx.request({
+          url: 'https://30paotui.com/user/wechat', 
+          data: {
+            appid: '你的小程序appid', 
+            secret: '你的小程序secret', 
+            code: res.code
+          }, 
+          success: function (response) {
+            var openid = response.data.openid; console.log('请求获取openid:' + openid);      //可以把openid存到本地，方便以后调用
+            wx.setStorageSync('openid', openid);
+            that.setData({
+              openid: "获取到的openid：" + openid
+            })
+          }
+        })
+      }
+    })
+  },
+  onLoad(){
+    console.log(numberReplace('29'))
+    this.setData({
+      number: numberReplace('29')
+    })
+  },
+  // getOpenid(){
+  //   console.log('1')
+  //    wx.login({
+  //      succes:res=>{
+  //        console.log('2')
+  //        console.log(res)
+  //        //  调用本地服务获取userId 及 本地库用户信息
+  //        wx.request({
+  //          url: 'https://iygltest.cvqzgcs.com/v2/travel/users/getUserInfo',
+  //          data: params,
+  //          method: 'POST',
+  //          header: {
+  //            "Content-Type": "application/json",
+  //            'token': res.respData
+  //          },
+  //          success: function (msg) {
+  //            console.log(msg.data.respData)
+  //            wx.setStorageSync('userMsgKK', msg.data.respData);
+  //            resolve(msg);
+  //          }
+  //        })
+  //      }
+  //    })
+  // },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -35,7 +93,7 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow () {
+  onShow() {
     console.log(getApp().globalData)
     if (
       wx.getStorageSync &&
--- utils/util.js
+++ utils/util.js
@@ -13,7 +13,25 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+const numberReplace = str => {
+  const NUMBER = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+  let strData = str.replace(/\d/gu, ($0) => {
+    return NUMBER[$0];
+  })
+  console.log(strData)
+  if (strData.length === 2) {
+    if (strData[0] === '一' && strData[1] === '零') {
+      strData = '十'
+    } else if (strData[0] === '一' && strData[1] !== '零') {
+      strData = '十' + strData[1]
+    } else {
+      strData = strData[0] + '十' + strData[1]
+    }
+  }
+  return strData
+};
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  numberReplace
 }
