c3cb2984f89b30b06263d6a42fa78d28582b0d72
di wang <wangdidi0920@gmail.com>
2019-09-26 09:59:46 +0800

    引入vant-weapp 库

--- pages/learn/catalog.js
+++ pages/learn/catalog.js
@@ -12,7 +12,8 @@ Page({
       { path: '/fenbao/packageA/pages/learn/five/five', name: '第五节：swiper组件和下拉菜单' },
       { path: '/fenbao/packageA/pages/learn/six/six', name: '第六节：跳转其他小程序和弹性布局' }, 
       { path: '/fenbao/packageA/pages/learn/seven/seven', name: '第七节：微信小程序支付' },
-      { path: '/fenbao/packageA/pages/learn/eight/eight', name: '第八节：倒计时和地区选择和变换主题' }
+      { path: '/fenbao/packageA/pages/learn/eight/eight', name: '第八节：倒计时和地区选择和变换主题' },
+      { path: '/fenbao/packageA/pages/learn/nine/nine', name: '第九节：使用vant-weapp库' }
       ]
   },
 
